e68c91243b762bb59d4283167fb54b1a82a0fbfd
Jackson Melcher <jacksonmelcher@protonmail.com>
2017-10-01 15:10:55 -0700

    Fixed sunset time.

--- test.js
+++ test.js
@@ -4,6 +4,6 @@ var SunCalc = require('suncalc');
 var times = SunCalc.getTimes(new Date(), 39.529633, -119.813803);
 
 // format sunrise time from the Date object
-var sunriseStr = (times.sunsetStart.getHours() - 12) + ':' + times.sunrise.getMinutes() + ' pm';
+var sunriseStr = (times.sunsetStart.getHours() - 12) + ':' + times.sunset.getMinutes() + ' pm';
 
 module.exports = sunriseStr
